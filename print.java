7786fd3a56c9e5a8a4fcb84dee46a75e6cfdd85f
Ayush Mundhe <mundheaayush5@gmail.com>
2023-08-25 18:05:51 +0530

    made changes in java file

--- print.java
+++ print.java
@@ -1,6 +1,6 @@
-public static print{
-       public static void main{
+public class print{
+       public static void main(String[] args){
 
           System.out.println("Ayush file created first time");
-}
+     }
 }
